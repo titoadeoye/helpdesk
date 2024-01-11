9052032c65435296c7c01b6476d079b7957c8c13
ty-codes <titoadeoye00@gmail.com>
2024-01-10 16:56:00 -0800

    refactor: create ticket list

--- src/app/tickets/page.jsx
+++ src/app/tickets/page.jsx
@@ -1,3 +1,18 @@
+import TicketList from "./TicketList";
+
 export default function Tickets() {
-  return <div>Tickets</div>;
+  return (
+    <main>
+      <nav>
+        <div>
+          <h2>Tickets</h2>
+          <p>
+            <small>Currently open tickets</small>
+          </p>
+        </div>
+      </nav>
+
+      <TicketList />
+    </main>
+  );
 }
